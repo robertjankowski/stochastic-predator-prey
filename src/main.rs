e98126511866f75218daa781c7a6d6fee786df1a
robertjankowski <robjanw11@wp.pl>
2020-03-31 21:34:37 +0200

    test rand crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use rand_distr::{Distribution, Normal};
+
 fn main() {
-    println!("Hello, world!");
+    let sigma = 1.4;
+    let normal = Normal::new(0.0, sigma).unwrap();
+    let v = normal.sample(&mut rand::thread_rng());
+    println!("{} is generated from N(0, {})", v, sigma);
 }
