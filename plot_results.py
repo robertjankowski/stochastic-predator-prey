8d0b88b7a84554f7f98f2c54f4a27b35befac746
robertjankowski <robjanw11@wp.pl>
2020-04-25 18:08:53 +0200

    working on plotting results

--- plot_results.py
+++ plot_results.py
@@ -3,15 +3,17 @@ import glob
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from matplotlib import animation
+from matplotlib import animation, rcParams, cycler
+
+cmap = plt.cm.jet
 
 
 def get_dt_from_file(filename: str):
-    dt = filename.split('/')[-1]
-    return float(dt.split('_alpha')[0].split('=')[-1])
+    dt = filename.split("/")[-1]
+    return float(dt.split("_alpha")[0].split("=")[-1])
 
 
-def load_datasets(folder: str):
+def load_datasets(folder: str, n_obs: int) -> list:
     files = glob.glob(folder + "/*.csv")
     if len(files) < 1:
         print(f"Not found datasets in [{folder}]")
@@ -19,12 +21,13 @@ def load_datasets(folder: str):
     dfs = []
     for filename in files:
         df = pd.read_csv(filename, index_col=None, header=0)
-        df.columns = ['prey', 'predator']
+        df.columns = ["prey", "predator"]
         dt = get_dt_from_file(filename)
         time = np.arange(len(df.index) * dt, step=dt)
-        dfs.append(pd.DataFrame(
-            {'time': time, 'prey': df.prey, 'predator': df.predator}))
-    return dfs
+        dfs.append(
+            pd.DataFrame({"time": time, "prey": df.prey, "predator": df.predator})
+        )
+    return dfs[:n_obs]
 
 
 def get_x_lim(df):
@@ -40,42 +43,81 @@ def get_y_lim(dfs):
     return np.min(y_min), np.max(y_max)
 
 
-def animate_results(folder: str, anim_name: str):
-    dfs = load_datasets(folder)
+def animate_results(folder: str, anim_name: str, n_obs: int):
+    dfs = load_datasets(folder, n_obs)
 
     def animate(i):
         plt.clf()
         plt.xlim(get_x_lim(dfs[0]))
         plt.ylim(get_y_lim(dfs))
         for num, df in enumerate(dfs[:5]):
-            plt.plot(df.time[:(i * 10)], df.prey[:(i * 10)],
-                     label=f'prey {num}')
-            plt.plot(df.time[:(i * 10)], df.predator[:(i * 10)],
-                     label=f'predator {num}')
-        plt.legend(loc='upper left')
-        plt.xlabel('time')
-        plt.ylabel('population')
+            plt.plot(df.time[: (i * 10)], df.prey[: (i * 10)], label=f"prey {num}")
+            plt.plot(
+                df.time[: (i * 10)], df.predator[: (i * 10)], label=f"predator {num}"
+            )
+        plt.legend(loc="upper left")
+        plt.xlabel("time")
+        plt.ylabel("population")
+
     fig, ax = plt.subplots(figsize=(10, 8))
-    anim = animation.FuncAnimation(fig, animate, frames=(int)(len(
-        dfs[0].time) / 10), interval=10, repeat=True)
-    anim.save(anim_name + '.gif', writer='imagemagick')
+    anim = animation.FuncAnimation(
+        fig, animate, frames=(int)(len(dfs[0].time) / 10), interval=10, repeat=True
+    )
+    anim.save(anim_name + ".gif", writer="imagemagick")
 
 
-def plot_results(folder: str):
-    dfs = load_datasets(folder)
+def plot_results_deterministic(df_deterministic):
+    plt.plot(
+        df_deterministic.time,
+        df_deterministic.prey,
+        label="deterministic prey",
+        color="black",
+        linewidth=2.5,
+    )
+    plt.plot(
+        df_deterministic.time,
+        df_deterministic.predator,
+        label="deterministic predator",
+        color="red",
+        linewidth=2.5,
+    )
+
+
+def plot_results(stochastic_folder: str, deterministic_folder: str, n_obs: int):
+    dfs = load_datasets(stochastic_folder, n_obs)
     for i, df in enumerate(dfs):
-        plt.plot(df.time, df.prey, label=f'prey {i + 1}')
-        plt.plot(df.time, df.predator, label=f'predator {i + 1}')
-    plt.xlabel('t')
-    plt.ylabel('population')
+        plt.plot(df.time, df.prey, label=f"stochastic prey {i + 1}")
+        plt.plot(df.time, df.predator, label=f"stochastic predator {i + 1}")
+    df_deterministic = load_datasets(deterministic_folder, 1)[0]
+    plot_results_deterministic(df_deterministic)
+    plt.xlabel("t")
+    plt.ylabel("population")
     plt.legend()
     plt.show()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Plot simulation results')
-    parser.add_argument('--folder_name', type=str,
-                        help='Folder name', required=True)
+    parser = argparse.ArgumentParser(description="Plot simulation results")
+    parser.add_argument(
+        "--stochastic_folder_name",
+        type=str,
+        help="Name of folder with stochastic solution",
+        required=True,
+    )
+    parser.add_argument(
+        "--n_obs_stochastic", type=int, help="Number of stochastic runs", required=True
+    )
+    parser.add_argument(
+        "--deterministic_folder_name",
+        type=str,
+        help="Name of folder with deterministic solution",
+        required=True,
+    )
+
     results = parser.parse_args()
-    # plot_results(results.folder_name)
-    animate_results(results.folder_name, 'stochastic')
+    stochastic_folder = results.stochastic_folder_name
+    n_obs = results.n_obs_stochastic
+    deterministic_folder = results.deterministic_folder_name
+
+    plot_results(stochastic_folder, deterministic_folder, n_obs)
+    # animate_results(results.folder_name, 'stochastic')
--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,13 @@ use simulation::{LotkaVolterraParameters, LotkaVolterraSimulation, SimulationPar
 fn main() {
     let lvp = LotkaVolterraParameters::new(1.5, 1.0, 1.0, 3.0);
     let sp = SimulationParameters::new(0.0, 15.0, 1000);
-    let mut lv_simulation = LotkaVolterraSimulation::new(&sp, lvp);
-    let n_iteration = 10;
-    let initial_x = 5.0;
-    let initial_y = 3.0;
     let sigma_x = 0.1;
     let sigma_y = 0.2;
-    lv_simulation.run_stochastic(n_iteration, initial_x, initial_y, sigma_x, sigma_y);
+    let mut lv_simulation = LotkaVolterraSimulation::new(&sp, lvp, sigma_x, sigma_y);
+    let n_iteration = 10;
+    let initial_x = 3.0;
+    let initial_y = 3.0;
+    lv_simulation.run_stochastic(n_iteration, initial_x, initial_y);
     // lv_simulation.run_deterministic(n_iteration, initial_x, initial_y);
-<<<<<<< HEAD
-    lv_simulation.save_data("stochastic2");
-=======
-    lv_simulation.save_data("stochastic_x=3_y=3");
->>>>>>> master
+    lv_simulation.save_data("stochastic=3_y=3");
 }
